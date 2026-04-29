45c99e67457955ba00ac709ccb4d7f94bb0e7ab5
Peter Le <rthinh2002@gmail.com>
2026-04-29 23:30:28 +0930

    Fix lint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-﻿import './App.css';
+import './App.css';
 import { useState, useEffect, useRef } from 'react';
 import { experiences } from './data/constants';
 import ProjectModal from './components/ProjectModal';
